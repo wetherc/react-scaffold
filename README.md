165b059f26cc0a70da1afea2f793c28a97630650
Christopher Wetherill <git@tbmh.org>
2023-01-10 20:26:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# React scaffolding
+
+This is meant to provide a simple scaffold to get up and running with a standalone Apollo server and a Postgres database as a dummy backend for a React app to interact with
+
+## Prerequisites
+
+  - Docker
+  - Docker Compose
+
+## Usage
+
+```
+$ docker compose up
+```
---
+++ apollo-server/src/index.ts
@@ -0,0 +1,45 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
