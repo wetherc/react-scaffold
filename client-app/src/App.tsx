06f49a5aacc8db3d3a8667a524e1a39bd71ea6a1
wetherc <1910269+wetherc@users.noreply.github.com>
2023-01-15 16:25:04 -0500

    unstyled query dump

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,27 +1,65 @@
-import { useState } from "react";
 import Header from "@cloudscape-design/components/header";
 import Container from "@cloudscape-design/components/container";
+import ContentLayout from "@cloudscape-design/components/content-layout";
 import SpaceBetween from "@cloudscape-design/components/space-between";
-import Input from "@cloudscape-design/components/input";
-import Button from "@cloudscape-design/components/button";
+import Link from "@cloudscape-design/components/link";
+import { useQuery, gql } from '@apollo/client';
+
+const GET_BOOKS = gql`
+  query ListBooks {
+    listBooks {
+      author {
+        lastName
+        firstName
+      }
+      publicationDate
+      title
+    }
+  }
+`;
+
+function GetBooks() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.listBooks.map(({ title, publicationDate, author }) => (
+    <ol>
+      <li>{title}</li>
+      <li>{publicationDate}</li>
+      <li>{author.lastName}, {author.firstName}</li>
+    </ol>
+  ));
+}
 
 export default function App() {
-  const [value, setValue] = useState("");
-
   return (
-    <SpaceBetween size="m">
-      <Header variant="h1">Hello World!</Header>
-
-      <Container>
-        <SpaceBetween size="s">
-          <span>Start editing to see some magic happen</span>
-          <Input
-            value={value}
-            onChange={(event) => setValue(event.detail.value)}
-          />
-          <Button variant="primary">Click me</Button>
+    <ContentLayout
+      header={
+        <SpaceBetween size="m">
+          <Header
+            variant="h1"
+            info={<Link>Info</Link>}
+            description="This is a generic library of some Sci-Fi authors and their works."
+          >
+            Sci-Fi Library
+          </Header>
         </SpaceBetween>
+      }
+    >
+      <Container
+        header={
+          <Header
+            variant="h2"
+            description="A selection of currently available books"
+          >
+            Selected Titles
+          </Header>
+        }
+      >
+        <GetBooks />
       </Container>
-    </SpaceBetween>
+    </ContentLayout>
   );
 }
