87263b6831be348453b8d79b8d3181e3b09b1011
wetherc <1910269+wetherc@users.noreply.github.com>
2023-01-16 11:12:01 -0500

    minimal ui styling

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,21 @@
-import Header from "@cloudscape-design/components/header";
-import Container from "@cloudscape-design/components/container";
-import ContentLayout from "@cloudscape-design/components/content-layout";
-import SpaceBetween from "@cloudscape-design/components/space-between";
-import Link from "@cloudscape-design/components/link";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { useQuery, gql } from '@apollo/client';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const GET_BOOKS = gql`
   query ListBooks {
@@ -25,41 +37,63 @@ function GetBooks() {
   if (error) return <p>Error : {error.message}</p>;
 
   return data.listBooks.map(({ title, publicationDate, author }) => (
-    <ol>
-      <li>{title}</li>
-      <li>{publicationDate}</li>
-      <li>{author.lastName}, {author.firstName}</li>
-    </ol>
+    <Grid item key={title} xs={12} sm={6} md={4}>
+      <Card
+        sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+      >
+        <CardMedia
+          component="img"
+          sx={{
+            // 16:9
+            pt: '56.25%',
+          }}
+          image="https://source.unsplash.com/random"
+          alt="random"
+        />
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography>
+            {title} ({publicationDate}) by {author.lastName}, {author.firstName}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View</Button>
+          <Button size="small">Edit</Button>
+        </CardActions>
+      </Card>
+    </Grid>
   ));
 }
+const theme = createTheme();
 
 export default function App() {
   return (
-    <ContentLayout
-      header={
-        <SpaceBetween size="m">
-          <Header
-            variant="h1"
-            info={<Link>Info</Link>}
-            description="This is a generic library of some Sci-Fi authors and their works."
-          >
-            Sci-Fi Library
-          </Header>
-        </SpaceBetween>
-      }
-    >
-      <Container
-        header={
-          <Header
-            variant="h2"
-            description="A selection of currently available books"
-          >
-            Selected Titles
-          </Header>
-        }
-      >
-        <GetBooks />
-      </Container>
-    </ContentLayout>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Author Library
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid container spacing={4}>
+              <GetBooks />
+            </Grid>
+          </Container>
+        </Box>
+      </main>
+    </ThemeProvider>
   );
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
@@ -14,6 +13,7 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
